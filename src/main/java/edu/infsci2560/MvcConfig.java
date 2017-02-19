baed17ca2f190afd79eb5991fbf572cffd0e9c05
ginkgomp <lvyuopo@hotmail.com>
2017-02-18 20:12:13 -0500

    Add CSS for location page

--- src/main/java/edu/infsci2560/MvcConfig.java
+++ src/main/java/edu/infsci2560/MvcConfig.java
@@ -2,9 +2,13 @@ package edu.infsci2560;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+
 @Configuration
+@EnableWebMvc
 public class MvcConfig extends WebMvcConfigurerAdapter {
 
     @Override
@@ -14,5 +18,13 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
         registry.addViewController("/locations").setViewName("locations");
         registry.addViewController("/login").setViewName("login");
     }
+    
+    @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+      
+    registry.addResourceHandler("/**").addResourceLocations("classpath:/META-INF/resources/", "classpath:/resources/",
+		"classpath:/static/", "classpath:/public/");  
+  }
 
 }
